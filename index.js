0c8ee70615cfded0fbbd1fdd661520c3a08dc946
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2018-05-15 10:40:58 +0900

    update devDependencies
    Highlights:
    
    * update ESLint from v3 to v4
    * update @shinnn/eslint-config and follow its rules, for exmaple tab indent
    * introduce rollup-config-module to adapt to .mjs-based ecosystem

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+'use strict';
+
 /*!
  * changelog-filename-regex | MIT (c) Shinnosuke Watanabe
  * https://github.com/shinnn/changelog-filename-regex
 */
-module.exports = /^(?:(?:update|change|release)(?:s|[ _-]*(?:logs?|histor(?:y|ies)))|histor(?:y|ies)|release[ _-]*notes?)(?:\.[A-Za-z0-9]+)?$/i;
+var index = /^(?:(?:update|change|release)(?:s|[ _-]*(?:logs?|histor(?:y|ies)))|histor(?:y|ies)|release[ _-]*notes?)(?:\.[A-Za-z0-9]+)?$/i;
+
+module.exports = index;
--- test.js
+++ test.js
@@ -1,86 +1,84 @@
 'use strict';
 
-const requireFromString = require('require-from-string');
-const rollup = require('rollup');
+const changelogFilenameRegex = require('.');
 const test = require('ava');
 
-function runTest(changelogFilenameRegex, description) {
-  test(`${description} exposes a regex that matches CHANGELOG-ish filenames.`, t => {
-    [
-      'updates',
-      'updatelog',
-      'updatelogs',
-      'updatehistory',
-      'updatehistories',
-      'update log',
-      'update logs',
-      'update history',
-      'update histories',
-      'update-log',
-      'update_log',
-      'update - log',
-      'update  log',
-      'update--log',
-      'changes',
-      'changelog',
-      'changelogs',
-      'changehistory',
-      'changehistories',
-      'releases',
-      'releaselog',
-      'releaselogs',
-      'releasehistory',
-      'releasehistories',
-      'history',
-      'histories',
-      'releasenote',
-      'releasenotes',
-      'release notes',
-      'release-note',
-      'release_note',
-      'release - note',
-      'release  note',
-      'release--note',
-      'ChangeLog',
-      'CHANGELOG',
-      'chanGeLog',
-      'changelog.md',
-      'changelog.1',
-      'changelog.TXT',
-      'changelog.TXT'
-    ].forEach(str => t.deepEqual(Array.from(changelogFilenameRegex.exec(str)), [str]));
+test('match CHANGELOG-ish filenames.', t => {
+	for (const str of [
+		'updates',
+		'updatelog',
+		'updatelogs',
+		'updatehistory',
+		'updatehistories',
+		'update log',
+		'update logs',
+		'update history',
+		'update histories',
+		'update-log',
+		'update_log',
+		'update - log',
+		'update  log',
+		'update--log',
+		'changes',
+		'changelog',
+		'changelogs',
+		'changehistory',
+		'changehistories',
+		'releases',
+		'releaselog',
+		'releaselogs',
+		'releasehistory',
+		'releasehistories',
+		'history',
+		'histories',
+		'releasenote',
+		'releasenotes',
+		'release notes',
+		'release-note',
+		'release_note',
+		'release - note',
+		'release  note',
+		'release--note',
+		'ChangeLog',
+		'CHANGELOG',
+		'chanGeLog',
+		'changelog.md',
+		'changelog.1',
+		'changelog.TXT',
+		'changelog.TXT'
+	]) {
+		t.deepEqual(Array.from(changelogFilenameRegex.exec(str)), [str]);
+	}
+});
 
-    [
-      'foo',
-      'update',
-      'change',
-      'release',
-      'change　log',
-      'change\nlog',
-      'change~log',
-      'change.log',
-      'changedog',
-      'changelog.',
-      'changel-og.',
-      'changeelog.',
-      'change-',
-      '-log',
-      'historys',
-      'changelog. md',
-      'changelog.m d',
-      'changelog.m^d',
-      'changelog.µd',
-      'changelog.md.txt',
-      ' changelog',
-      'changelog ',
-      ' changelog ',
-      'changelog\n'
-    ].forEach(str => t.is(changelogFilenameRegex.exec(str), null));
-  });
-}
-
-runTest(require('.'), 'require(\'changelog-filename-regex\')');
-
-rollup.rollup({entry: require('./package.json')['jsnext:main']}).then(bundle => {
-  runTest(requireFromString(bundle.generate({format: 'cjs'}).code), 'Module exports');
+test('don\'t match non-CHANGELOG filenames.', t => {
+	for (const str of [
+		'foo',
+		'update',
+		'change',
+		'release',
+		'change　log',
+		'change\nlog',
+		'change~log',
+		'change.log',
+		'changedog',
+		'changelog.',
+		'changel-og.',
+		'changeelog.',
+		'change-',
+		'-log',
+		'historys',
+		'changelog. md',
+		'changelog.m d',
+		'changelog.m^d',
+		'changelog.µd',
+		'changelog.md.txt',
+		' changelog',
+		'changelog ',
+		' changelog ',
+		'changelog\n',
+		'\0changelog'
+	]) {
+		t.is(changelogFilenameRegex.exec(str), null);
+	}
 });
