6078fb5e12a5f86a1227f093da40c31d2c601c90
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2018-05-18 10:46:33 +0900

    update devDependencies

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /*!
- * changelog-filename-regex | MIT (c) Shinnosuke Watanabe
+ * changelog-filename-regex | ISC (c) Shinnosuke Watanabe
  * https://github.com/shinnn/changelog-filename-regex
 */
 var index = /^(?:(?:update|change|release)(?:s|[ _-]*(?:logs?|histor(?:y|ies)))|histor(?:y|ies)|release[ _-]*notes?)(?:\.[A-Za-z0-9]+)?$/i;
